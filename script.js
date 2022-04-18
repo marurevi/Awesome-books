5308106861747d5d8eb7dc5f2bb9748478cd77c5
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-04-18 15:37:04 -0300

    add books dinamically

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
 const bookShelf = document.getElementById('bookshelf');
-const book 
+
+const libro1 = { nombre: 'Nombre1', autor: 'Autor1' };
+const libro2 = { nombre: 'Nombre2', autor: 'Autor2' };
+const books = [libro1, libro2];
+const libro = [];
+for (let i = 0; i < books.length; i++) {
+  const nombre = document.createElement('div');
+  nombre.innerHTML = books[i].nombre;
+  bookShelf.appendChild(nombre);
+  const autor = document.createElement('div');
+  autor.innerHTML = books[i].autor;
+  bookShelf.appendChild(autor);
+  const removeButton = document.createElement('button');
+  bookShelf.appendChild(removeButton);
+  removeButton.innerHTML = 'Remove';
+}
