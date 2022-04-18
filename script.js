6013ab7469b0ba69ad85fdff73348afb87dff8b8
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-04-18 17:17:40 -0300

    store and retrieve data

--- script.js
+++ script.js
@@ -4,53 +4,53 @@ const bookShelf = document.getElementById('bookshelf');
 
 const book1 = { name: 'Name1', author: 'Author1' };
 const book2 = { name: 'Name2', author: 'Author2' };
-const books = [book1, book2];
+let books = [];
 
 const addBtn = document.getElementById('add');
 const title = document.getElementById('newName');
 const author = document.getElementById('newAuthor');
 
-class Book {
-    constructor (name_book, author_book) {
-        this.name_book = name_book;
-        this.author_book = author_book;
-    }
+function savedata() {
+  console.log('savedata');
+  localStorage.setItem('booksdata', JSON.stringify(books));
 }
 
-for (let i = 0; i < books.length; i++) {
-  const name = document.createElement('div');
-  name.innerHTML = books[i].name;
-  bookShelf.appendChild(name);
-  const author = document.createElement('div');
-  author.innerHTML = books[i].author;
-  bookShelf.appendChild(author);
-  const removeButton = document.createElement('button');
-  bookShelf.appendChild(removeButton);
-  removeButton.innerHTML = 'Remove';
-  removeButton.className = 'remove';
-  removeButton.id = 'remove'+`${i}`;
-  const separator = document.createElement('hr');
-  bookShelf.appendChild(separator);
+function retrievedata() {
+  if (localStorage.getItem('booksdata') != null) {
+    books = JSON.parse(localStorage.getItem('booksdata'));
+  }
+  for (let i = 0; i < books.length; i++) {
+    const bookname = document.createElement('div');
+    bookname.innerHTML = books[i].name;
+    bookShelf.appendChild(bookname);
+    const bookauthor = document.createElement('div');
+    bookauthor.innerHTML = books[i].author;
+    bookShelf.appendChild(bookauthor);
+    const removeButton = document.createElement('button');
+    bookShelf.appendChild(removeButton);
+    removeButton.innerHTML = 'Remove';
+    removeButton.className = 'remove';
+    removeButton.id = `remove${i}`;
+    const separator = document.createElement('hr');
+    bookShelf.appendChild(separator);
+  }
+}
+
+console.log(books);
+class Book {
+  constructor(name, author) {
+    this.name = name;
+    this.author = author;
+  }
+}
+
+function addNewBook() {
+  // const book = new Book(title.value, author.value);
+  const book = { name: title.value, author: author.value };
+  books.push(book);
+  savedata();
 }
 
 addBtn.addEventListener('click', addNewBook);
 
-function addNewBook() {
-  const book = new Book (title.value, author.value);
-  books.push(book);
-  const name = document.createElement('div');
-  name.innerHTML = title.value;
-  bookShelf.appendChild(name);
-  const author = document.createElement('div');
-  author.innerHTML = author.value;
-  bookShelf.appendChild(author);
-  const removeButton = document.createElement('button');
-  bookShelf.appendChild(removeButton);
-  removeButton.innerHTML = 'Remove';
-  removeButton.className = 'remove';
-  removeButton.id = 'remove'+`${books.length}`;
-  const separator = document.createElement('hr');
-  bookShelf.appendChild(separator);
-}
-
-
+window.addEventListener('load', retrievedata());
